07faa1e4cd175d7d0eb07e9b18fcc9a3baaf9d86
Caroline Genzel <carolinegenzel@Mac.lan>
2024-11-12 20:31:43 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	   String name= args[0];
+           int cheers=Integer.parseInt(args[1]);
+           String anLetters = "AEFHILMNORSX";
+          for(int j=0;j<name.length();j++) {
+                char ch= name.charAt(j);
+                if (anLetters.indexOf(ch)!= -1)
+                {
+                        System.out.println("Give me an "+ name.charAt(j)+": "+ name.charAt(j)+"!");
+                }
+                else System.out.println("Give me a  "+ name.charAt(j)+": "+ name.charAt(j)+"!");
+           
+           }
+           System.out.println("What does that spell?");
+           for(int i=0;i<cheers;i++){
+                System.out.println(name+"!!!");
+           }
         }
 }
